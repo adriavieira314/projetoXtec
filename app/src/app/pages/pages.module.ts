1d7c0ffb1c7051fa91e445c85d22a8873c01546e
Adria Vieira <adriavieira731@gmail.com>
2021-01-13 04:09:59 -0400

    modulos das mascaras adicionadas

--- app/src/app/pages/pages.module.ts
+++ app/src/app/pages/pages.module.ts
@@ -3,28 +3,35 @@ import { HttpClientModule } from "@angular/common/http";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { TextMaskModule } from 'angular2-text-mask';
+import { CurrencyMaskModule } from "ng2-currency-mask";
 
 import { CalculadoraComponent } from "./calculadora/calculadora.component";
 import { InterfaceComponent } from "./interface-calculadora/interface.component";
 import { CardsComponent } from "./cardsProdutos/cards.component";
+import { BotoesAlternativosComponent } from "./botoesAlternativos/botoesAlternativos.component";
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         HttpClientModule,
+        TextMaskModule,
+        CurrencyMaskModule
     ],
     // devo declarar no declarations para o PagesModule enxergar o componente
     declarations: [
         CalculadoraComponent,
         InterfaceComponent,
-        CardsComponent
+        CardsComponent,
+        BotoesAlternativosComponent
     ],
     // devo declarar no exports para quem for importar PagesModule enxergar o componente
     exports: [
         CalculadoraComponent,
         InterfaceComponent,
-        CardsComponent
+        CardsComponent,
+        BotoesAlternativosComponent
     ],
 })
 export class PagesModule { }
