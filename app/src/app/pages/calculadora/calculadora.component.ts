36b02280fa6a6452d4d7ca11da7b3e7adcd04afb
Adria Vieira <adriavieira731@gmail.com>
2021-01-13 01:32:14 -0400

    funcao dos produtos adicionada e funcionando

--- app/src/app/pages/calculadora/calculadora.component.ts
+++ app/src/app/pages/calculadora/calculadora.component.ts
@@ -1,24 +1,9 @@
 // cada novo componente devo declarar no app.module.ts
-import { AfterViewInit, Component, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
-import { InterfaceComponent } from '../interface-calculadora/interface.component';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-calculadora',
     templateUrl: './calculadora.component.html',
     styleUrls: ['./calculadora.component.css']
 })
-export class CalculadoraComponent implements AfterViewInit, OnChanges {
-    
-    @ViewChild(InterfaceComponent) child;
-    
-    teste: boolean;
-
-    ngAfterViewInit(): void {
-        this.teste = this.child.teste;
-        console.log(this.teste);
-    }
-
-    ngOnChanges(changes: SimpleChanges): void {
-        this.teste = this.child.teste;
-    }
-}
+export class CalculadoraComponent { }
--- app/src/app/pages/interface-calculadora/interface.component.ts
+++ app/src/app/pages/interface-calculadora/interface.component.ts
@@ -9,7 +9,7 @@ declare var $:any;
   styleUrls: ['./interface.component.css']
 })
 export class InterfaceComponent implements OnInit {
-  teste = [];
+  produtos = [];
   today = new Date();
   monName = new Array ("JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC");
   cdInput: string = 'cep';
@@ -18,12 +18,13 @@ export class InterfaceComponent implements OnInit {
   mascara: string = "00000-000"
   cepInformado = false;
   grupo = 'Grupo';
+  pronto: boolean = false;
 
   inputCEP: string;
   inputValor: number;
   energia: number;
   potPico: number;
-  selectedPainel: any = '410';
+  selectedPainel: any = '410W';
   selectedEstrutura: any = 'METALICA';
   potPainel: any;
   paineis: number;
@@ -31,6 +32,7 @@ export class InterfaceComponent implements OnInit {
   investimento: number;
   result: IrradiacaoSolar[] = [];
   irradiacaoSolar: number;
+  precoKit: number;
 
   energiaText: any;
   potPicoText: any;
@@ -42,7 +44,6 @@ export class InterfaceComponent implements OnInit {
   ngOnInit(): void {
     this.painel();
     this.convertCSV();
-    this.pegaJSON();
   }
 
   onClick(id:string) {
@@ -58,7 +59,7 @@ export class InterfaceComponent implements OnInit {
       this.mascara = "R$";
       this.inputValor = 0;
     } else {
-      // $scope.mascara = "0000-000";
+      this.mascara = "0000-000";
       this.cdInput = "cep";
     }
   }
@@ -70,12 +71,19 @@ export class InterfaceComponent implements OnInit {
       if (validacep.test(this.inputCEP)) {
         this.interfaceService.getCepApi(this.inputCEP).subscribe(
           dados => {
-            this.local = dados.uf + " - " + dados.localidade;
-            this.buscaIrradiacao(dados.localidade);
-            this.cepInformado = true;
-            this.cdInput = "reais";
-            this.inputValor = 0;
-            this.mascara = "R$";
+            if (dados.localidade) {
+              this.local = dados.uf + " - " + dados.localidade;
+              this.buscaIrradiacao(dados.localidade);
+              this.cepInformado = true;
+              this.cdInput = "reais";
+              this.inputValor = 0;
+              this.mascara = "R$";
+            } else {
+              console.log(dados);
+              this.cepInformado = false;
+              this.apagar();
+              alert('CEP não encontrado.');
+            }
           },
           erro => {
             console.log(erro);
@@ -89,214 +97,284 @@ export class InterfaceComponent implements OnInit {
     }
   }
 
-    calculoEnergia() {
-      if (this.selectedValor === "consumo") {
-        this.energia = this.inputValor;
-        this.energiaText = this.energia;
-      } else {
-        var energiaCalc = this.inputValor/0.92;
-        this.energia = Math.floor(energiaCalc);
-        this.energiaText = this.energia;
-      }
+  calculoEnergia() {
+    if (this.selectedValor === "consumo") {
+      this.energia = this.inputValor;
+      this.energiaText = this.energia;
+    } else {
+      var energiaCalc = this.inputValor/0.92;
+      this.energia = Math.floor(energiaCalc);
+      this.energiaText = this.energia;
+    }
+  }
+
+  calculoPotenciaPico() {
+    var potPicoCalc = this.energia/(30 * this.irradiacaoSolar * 0.80);
+    this.potPico = parseFloat(potPicoCalc.toFixed(2));
+    this.potPicoText = this.potPico;
+  }
+
+  painel(event:any = '410') {
+    if (event === '335') {
+      this.potPainel = 335/1000;
+
+    } else if (event === '410') {
+      this.potPainel = 410/1000;
+
+    } else {
+      this.potPainel = 440/1000;
     }
 
-    calculoPotenciaPico() {
-      var potPicoCalc = this.energia/(30 * this.irradiacaoSolar * 0.80);
-      this.potPico = parseFloat(potPicoCalc.toFixed(2));
-      this.potPicoText = this.potPico;
+  }
+
+  calculoQtdPaineis() {
+    var painel = this.potPico/this.potPainel;
+
+    this.paineis = Math.floor(painel);
+    if (this.paineis %  2 === 1) {
+      this.paineis++;
+      this.paineisText = this.paineis;
+
+    } else {
+      this.paineisText = this.paineis;
     }
+  }
 
-    painel(event:any = '410') {
-      if (event === '335') {
-        this.potPainel = 335/1000;
+  calculoArea() {
+    var areaCalc = 2.03 * this.paineis;
+    this.area = parseFloat(areaCalc.toFixed(2));
+    this.areaText = this.area;
+  }
 
-      } else if (event === '410') {
-        this.potPainel = 410/1000;
+  tipoGrupo() {
+    if (this.potPico >= 104) {
+      this.grupo = "Grupo A";
+      alert('Grupo A');
 
-      } else {
-        this.potPainel = 440/1000;
-      }
+    } else {
+      this.cdInput = "carregando";
+      this.grupo = "Grupo B";
+      this.pegaJSON();
+    }
+  }
+
+  valorInvestimento = function() {
+    var number = 0;
+    if (this.potPico > 0 && this.potPico < 20) {
+      number = 2.000 + 4.346 + this.precoKit;
+      this.investimento = parseFloat(number.toFixed(3));
 
     }
+    if (this.potPico >= 20 && this.potPico < 25) {
+      number = 2.000 + 8.406 + this.precoKit;
+      this.investimento = parseFloat(number.toFixed(3));
 
-    calculoQtdPaineis() {
-      var painel = this.potPico/this.potPainel;
-
-      this.paineis = Math.floor(painel);
-      if (this.paineis %  2 === 1) {
-        this.paineis++;
-        this.paineisText = this.paineis;
-
-      } else {
-        this.paineisText = this.paineis;
-      }
     }
+    if (this.potPico >= 25 && this.potPico < 35) {
+      number = 2.500 + 10.098 + this.precoKit;
+      this.investimento = parseFloat(number.toFixed(3));
 
-    calculoArea() {
-      var areaCalc = 2.03 * this.paineis;
-      this.area = parseFloat(areaCalc.toFixed(2));
-      this.areaText = this.area;
     }
+    if (this.potPico >= 35 && this.potPico < 46) {
+      number = 2.500 + 12.337 + this.precoKit;
+      this.investimento = parseFloat(number.toFixed(3));
 
-    tipoGrupo() {
-      if (this.potPico >= 104) {
-        this.grupo = "Grupo A";
-        alert('Grupo A')
-      } else {
-        // this.carregando = true;
-        this.cdInput = "carregando";
-        this.grupo = "Grupo B";
-        // getXML();
-      }
     }
+    if (this.potPico >= 46 && this.potPico < 57) {
+      number = 3.500 + 14.670 + this.precoKit;
+      this.investimento = parseFloat(number.toFixed(3));
 
-    valorInvestimento = function() {
-      var number = 0;
-      if (this.potPico > 0 && this.potPico < 20) {
-        number = 2.000 + 4.346 + this.precoKit;
-        this.investimento = parseFloat(number.toFixed(3));
-        console.log(this.investimento);
-
-      }
-      if (this.potPico >= 20 && this.potPico < 25) {
-        number = 2.000 + 8.406 + this.precoKit;
-        this.investimento = parseFloat(number.toFixed(3));
-
-      }
-      if (this.potPico >= 25 && this.potPico < 35) {
-        number = 2.500 + 10.098 + this.precoKit;
-        this.investimento = parseFloat(number.toFixed(3));
-
-      }
-      if (this.potPico >= 35 && this.potPico < 46) {
-        number = 2.500 + 12.337 + this.precoKit;
-        this.investimento = parseFloat(number.toFixed(3));
-
-      }
-      if (this.potPico >= 46 && this.potPico < 57) {
-        number = 3.500 + 14.670 + this.precoKit;
-        this.investimento = parseFloat(number.toFixed(3));
-
-      }
-      if (this.potPico >= 57 && this.potPico < 88) {
-        number = 4.000 + 18.090 + this.precoKit;
-        this.investimento = parseFloat(number.toFixed(3));
-
-      }
-      if (this.potPico >= 88 && this.potPico < 104) {
-        number = 4.500 + 23.588 + this.precoKit;
-        this.investimento = parseFloat(number.toFixed(3));
-      }
     }
+    if (this.potPico >= 57 && this.potPico < 88) {
+      number = 4.000 + 18.090 + this.precoKit;
+      this.investimento = parseFloat(number.toFixed(3));
 
-    resultado() {
-      this.calculoEnergia();
-      this.calculoPotenciaPico();
-      this.calculoQtdPaineis();
-      this.calculoArea();
-      this.tipoGrupo();
     }
-
-    apagar() {
-      // this.topTres = [];
-      // this.preco = [];
-      // this.carregando = true;
-      // this.precoKit = 0;
-
-      // this.cdInput = "cep";
-      // this.mascara = "0000-000";
-      // this.inputCEP = "";
-
-      // this.local = 'UF';
-      // this.energia = 0;
-      // this.potPico = 0;
-      // this.paineis = 0;
-      // this.area = 0;
-      // this.inputValor = 0;
-      // this.investimento = 0;
-      // this.energiaText = "";
-      // this.potPicoText = "";
-      // this.paineisText = "";
-      // this.areaText = "";
-
-      // this.grupo = "Grupo";
-      // this.cepInformado = false;
-      console.log(this.teste);
+    if (this.potPico >= 88 && this.potPico < 104) {
+      number = 4.500 + 23.588 + this.precoKit;
+      this.investimento = parseFloat(number.toFixed(3));
     }
+  }
 
-    convertCSV() {
-      var dados = [];
-      $.ajax({
-        type: "GET",
-        url: "assets/irradiacao/irradiacaoSolar.csv",
-        dataType: "text",
-        success: function(csv:any) {
-          var lines = csv.split("\n");
+  resultado() {
+    this.calculoEnergia();
+    this.calculoPotenciaPico();
+    this.calculoQtdPaineis();
+    this.calculoArea();
+    this.tipoGrupo();
+  }
 
-          for(let i = 0; i < lines.length; i++){
-            lines[i] = lines[i].replace(/\s/,'')//delete all blanks
+  apagar() {
+    this.produtos = [];
+    this.pronto = false;
+    this.precoKit = 0;
+
+    this.cdInput = "cep";
+    this.mascara = "0000-000";
+    this.inputCEP = "";
+
+    this.local = 'UF';
+    this.energia = 0;
+    this.potPico = 0;
+    this.paineis = 0;
+    this.area = 0;
+    this.inputValor = 0;
+    this.investimento = 0;
+    this.energiaText = "";
+    this.potPicoText = "";
+    this.paineisText = "";
+    this.areaText = "";
+
+    this.grupo = "Grupo";
+    this.cepInformado = false;
+    this.selectedPainel = '410W';
+    this.selectedEstrutura = 'METALICA';
+  
+  }
+
+  convertCSV() {
+    var dados = [];
+    $.ajax({
+      type: "GET",
+      url: "assets/irradiacao/irradiacaoSolar.csv",
+      dataType: "text",
+      success: function(csv:any) {
+        var lines = csv.split("\n");
+
+        for(let i = 0; i < lines.length; i++){
+          lines[i] = lines[i].replace(/\s/,'')//delete all blanks
+        }
+
+        var headers = lines[0].split(";");
+
+        for(var i = 1; i < lines.length; i++){
+          var obj = {};
+          var currentline = lines[i].split(";");
+
+          for(var j = 0; j < headers.length; j++){
+            obj[headers[j]] = currentline[j];
           }
 
-          var headers = lines[0].split(";");
+          dados.push(obj);
+        }
+      }
+    })
+    this.result = dados;
+    return this.result; //JavaScript object
+  }
 
-          for(var i = 1; i < lines.length; i++){
-            var obj = {};
-            var currentline = lines[i].split(";");
+  //formata o numero para um numero real colocando um ponto
+  formatarValor(valor:any) {
+    return valor.toLocaleString('pt-BR');
+  }
 
-            for(var j = 0; j < headers.length; j++){
-              obj[headers[j]] = currentline[j];
+  buscaIrradiacao(localidade:string) {
+    //pegando o mes em que o usuario entrou
+    var mes = this.monName[this.today.getMonth()];
+
+    for (let index = 0; index < this.result.length; index++) {
+      if (this.result[index].NAME === localidade) {
+        //pegando a irradiacao solar do mes
+        var valorIrradiacao = this.result[index][mes];
+        //convertendo para numero
+        var stringParaNumero = parseFloat(valorIrradiacao);
+
+        var valorFormatado = parseFloat(this.formatarValor(stringParaNumero));
+        this.irradiacaoSolar = parseFloat(valorFormatado.toFixed(2));
+      }
+    }
+  }
+
+  pegaJSON() {
+    this.produtos = [];
+    var preco = [];
+    this.interfaceService.getJSON().subscribe(dados => {
+      for (let i = 0; i < dados.produtos.produto.length; i++) {
+        if (dados.produtos.produto[i].atributos) { //verificando se possui a tag atributo
+          if (dados.produtos.produto[i].atributos[0].MARCA_PAINEL[0]) { //verificando se possui a tag marca_painel dentro da tag atributo
+            var string = dados.produtos.produto[i].atributos[0].MARCA_PAINEL[0]; //se sim, pegando a marca
+            var painel = string.split(" ");//transformando a string em array
+            
+            if (painel.includes(this.selectedPainel)) { //verificando se o array possui tal painel
+              //pegando o tipo de estrutura dos produtos com o painel selecionado
+              var tipo = dados.produtos.produto[i].atributos[0].TIPO_ESTRUTURA[0];
+              var estrutura = tipo.split(" ");
+              
+              if (estrutura.includes(this.selectedEstrutura)) {
+                preco.push(dados.produtos.produto[i].precoeup[0]);
+                
+                // verificando se valor tem mais de 12 caracteres e retirando-o do array
+                for (let i = 0; i < preco.length; i++) {
+                  if (preco[i].length === 12) {
+                    var elemento = preco.indexOf(preco[i]);
+                    preco.splice(elemento, 1);
+                  }
+                }
+
+                var sorting = preco.sort();
+
+                //adiciona aos array, os tres produtos com preços baixos
+                if (dados.produtos.produto[i].precoeup[0] === sorting[0]) {
+                  this.produtos.push({
+                    categoria: dados.produtos.produto[i].categoria[0],
+                    descricao: dados.produtos.produto[i].descricao[0],
+                    precoeup: dados.produtos.produto[i].precoeup[0],
+                    foto: dados.produtos.produto[i].foto[0],
+                    marca: dados.produtos.produto[i].marca[0],
+                    codigo: dados.produtos.produto[i].codigo[0]
+                  })
+                }
+                if (dados.produtos.produto[i].precoeup[0] === sorting[1]) {
+                  this.produtos.push({
+                    categoria: dados.produtos.produto[i].categoria[0],
+                    descricao: dados.produtos.produto[i].descricao[0],
+                    precoeup: dados.produtos.produto[i].precoeup[0],
+                    foto: dados.produtos.produto[i].foto[0],
+                    marca: dados.produtos.produto[i].marca[0],
+                    codigo: dados.produtos.produto[i].codigo[0]
+                  })
+                }
+                if (dados.produtos.produto[i].precoeup[0] === sorting[2]) {
+                  this.produtos.push({
+                    categoria: dados.produtos.produto[i].categoria[0],
+                    descricao: dados.produtos.produto[i].descricao[0],
+                    precoeup: dados.produtos.produto[i].precoeup[0],
+                    foto: dados.produtos.produto[i].foto[0],
+                    marca: dados.produtos.produto[i].marca[0],
+                    codigo: dados.produtos.produto[i].codigo[0]
+                  })
+                }
+                
+                //há casos com mais de um produto com o mesmo preco e isso causa um bug, esse código previne de ocorrer bug
+                // this.produtos = this.produtos.filter(function (a) {
+                //     return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
+                // }, Object.create(null));
+
+                //limita o tamanho do array a 3
+                this.produtos = this.produtos.slice(0,3);
+                
+                this.produtos = this.produtos.sort((a, b) => a.precoeup < b.precoeup ? -1 : a.precoeup > b.precoeup ? 1 : 0);
+                this.arrayPronta();
+              }
             }
-
-            dados.push(obj);
           }
-
-          // console.log(this.dados);
-          // return JSON.stringify(result); //JSON
-        }
-      })
-      this.result = dados;
-      return this.result; //JavaScript object
-    }
-
-    //formata o numero para um numero real colocando um ponto
-    formatarValor(valor:any) {
-      return valor.toLocaleString('pt-BR');
-    }
-
-    buscaIrradiacao(localidade:string) {
-      //pegando o mes em que o usuario entrou
-      var mes = this.monName[this.today.getMonth()];
-
-      for (let index = 0; index < this.result.length; index++) {
-        if (this.result[index].NAME === localidade) {
-          //pegando a irradiacao solar do mes
-          var valorIrradiacao = this.result[index][mes];
-          //convertendo para numero
-          var stringParaNumero = parseFloat(valorIrradiacao);
-
-          var valorFormatado = parseFloat(this.formatarValor(stringParaNumero));
-          this.irradiacaoSolar = parseFloat(valorFormatado.toFixed(2));
-          console.log(this.irradiacaoSolar);
         }
       }
-    }
+    })
+  }
 
-    pegaJSON() {
-      this.interfaceService.getJSON().subscribe(dados => {
-        for (let i = 0; i < dados.produtos.produto.length; i++) {
-          const element = dados.produtos.produto[i];
-          this.teste.push({
-            categoria: dados.produtos.produto[i].categoria[0],
-            descricao: dados.produtos.produto[i].descricao[0],
-            precoeup: dados.produtos.produto[i].precoeup[0],
-            foto: dados.produtos.produto[i].foto[0],
-            marca: dados.produtos.produto[i].marca[0],
-            codigo: dados.produtos.produto[i].codigo[0]
-          })
-          console.log(element);
-        }
-      })
-    }
+  arrayPronta() {
+    this.pronto = true;
+    this.cdInput = "grupo";
+    this.precoKit = parseFloat(this.produtos[0].precoeup);
+    this.valorInvestimento();
+  }
+
+  precoGerador(preco) {
+    $('html, body').animate({scrollTop: $("#marca-calc").offset().top}, 500);
+    this.precoKit = parseFloat(preco);
+    this.valorInvestimento();
+  }
 }
 
   
